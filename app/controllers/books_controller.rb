bb9a958cefe8de64506dbab288231f28ac6c9aa1
EC2 Default User <ec2-user@ip-172-31-3-69.ec2.internal>
2020-09-01 05:27:15 +0000

    [add] edit show

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,41 @@
+class BooksController < ApplicationController
+  def top
+  end
+
+  def index
+    @books = Book.all
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to books_path
+  end
+  
+  def update
+    book = Book.find(params[:id])
+    book.update
+    redirect_to books_path(book)
+  end
+  
+  def destroy
+  end
+  
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+end
