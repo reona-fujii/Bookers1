d013ed5cf9bda6d653f3d3994fc0c3e49e55b2d8
EC2 Default User <ec2-user@ip-172-31-3-69.ec2.internal>
2020-09-03 07:04:09 +0000

    [add]error message

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -16,22 +16,24 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    if book.save
+    @book = Book.new(book_params)
+    @books = Book.all
+    if @book.save
        flash[:notice] = "Book was successfully created."
-       redirect_to book_path(book)
+       redirect_to book_path(@book)
     else
-       redirect_to books_path
+      @books = Book.all
+       render "index"
     end
   end
 
   def update
-    book = Book.find(params[:id])
-    if book.update(book_params)
-      flash[:notice] = "Book was successfully updated."
-      redirect_to book_path(book)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+       flash[:notice] = "Book was successfully updated."
+       redirect_to book_path(book)
     else
-      render edit_book_path(book)
+      render "edit"
     end
   end
 
